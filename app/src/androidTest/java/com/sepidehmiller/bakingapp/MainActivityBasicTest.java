7f4be4baaccc16efd522d2526359b2ede9948012
Baghaii <baghaii@gmail.com>
2018-07-24 12:27:22 -0600

    Add two working Espresso tests to MainActivity.java.

--- app/src/androidTest/java/com/sepidehmiller/bakingapp/MainActivityBasicTest.java
+++ app/src/androidTest/java/com/sepidehmiller/bakingapp/MainActivityBasicTest.java
@@ -2,9 +2,10 @@ package com.sepidehmiller.bakingapp;
 
 import android.app.Activity;
 import android.app.Instrumentation;
-import android.content.Intent;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -13,12 +14,14 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.AllOf.allOf;
 
@@ -39,13 +42,22 @@ public class MainActivityBasicTest {
   }
 
   @Test
-  public void clickRecipe() {
-    onView(withId(R.id.main_card_view)).perform(click());
+  public void clickRecipe_CheckExtras() {
+    onView(withText("Nutella Pie")).perform(click());
     intended(allOf(
-        hasAction(Intent.ACTION_SENDTO),
         hasExtraWithKey(RecipeAdapter.RECIPE),
         hasExtraWithKey(RecipeAdapter.INGREDIENTS),
         hasExtraWithKey(RecipeAdapter.STEPS)
     ));
+  }
+
+  // http://blog.sqisland.com/2015/05/espresso-match-toolbar-title.html
+  @Test
+  public void checkTitle() {
+    onView(allOf(
+        isAssignableFrom(TextView.class),
+        withParent(isAssignableFrom(Toolbar.class))))
+        .check(matches(withText("Baking Time")));
+
   }
 }
